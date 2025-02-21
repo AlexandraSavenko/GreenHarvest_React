f2c42431a612d2a83cec3b02110bec8cd7aa6ae9
Sasha Savenko <sashasavenko3@gmail.com>
2025-02-21 16:27:59 +0200

    working on orderstep numbers

--- src/components/OrderStepsSection/OrderStepsSection.jsx
+++ src/components/OrderStepsSection/OrderStepsSection.jsx
@@ -1,22 +1,28 @@
 import OrderSteps_1x_min from "../assets/OrderSteps_1x_min.jpg"
 import OrderSteps_2x_min from "../assets/OrderSteps_2x_min.jpg"
-
+import css from "./OrderStepsSection.module.css"
 export default function OrderStepsSection () {
     return <div className="orderSteps">
-        <img srcSet={`${OrderSteps_2x_min}, ${OrderSteps_1x_min}`}
+        <div className="wrapper">
+            <img srcSet={`${OrderSteps_2x_min}, ${OrderSteps_1x_min}`}
          src={OrderSteps_1x_min} alt="a happy woman is slicing an apple" width={200} />
         <h2>TO ORDER YOUR <span>VEGETABLE</span> BASKET, SIMPLY FOLLOW THESE <span>EASY STEPS</span></h2>
     <p>Our baskets are assembled with care and delivered straight to your doorstep, so you can enjoy the taste of fresh fruit without ever leaving your home.</p>
-    <ul>
+    <ul className={css.orderSteps}>
         <li>
+            <span className={css.orderStepNum}>1</span>
             <p>Just choose <span>the vegetable</span> you want to order by clicking on the checkboxes <span>next to it</span> .</p>
         </li>
         <li>
+            <span className={css.orderStepNum}>2</span>
             <p>Click on <span>the basket</span>  and fill out the form.</p>
         </li>
         <li>
+            <span className={css.orderStepNum}>3</span>
             <p>Sit back and relax! Your <span>fresh vegetable</span> basket will be delivered.</p>
         </li>
     </ul>
+    </div>
+        
     </div>
 }
--- src/components/ReviewsSection/ReviewsSection.jsx
+++ src/components/ReviewsSection/ReviewsSection.jsx
@@ -1,10 +1,49 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Keyboard, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import 'swiper/css/bundle';
 import css from "./ReviewSection.module.css"
+import Ihor_min from "../assets/Ihor_min.jpg"
+import Victoria_min from "../assets/Victoria_min.jpg"
+import John_min from "../assets/John_min.jpg"
+
+
 export default function ReviewsSection () {
     return <div className={css.reviews}>
         <h2>REVIEWS FROM OUR <span>CUSTOMERS</span></h2>
         <p>Words of our customers: impressions and opinions about the
              quality of our services and the freshness of our products.</p>
-    <ul>
+             <Swiper 
+                   modules={[Keyboard, Pagination, Scrollbar, A11y]}
+
+      spaceBetween={50}
+      slidesPerView={1}
+      keyboard
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide className={css.slide}>
+        <img src={John_min} alt="John Wax shares his oppinion" />
+        <h3>John Wax</h3>
+            <p>These vegetables are my secret to delicious and healthy meals. 
+                The quality is always the highest level, the aroma and taste are enehanting. I advise every fan of real products!</p></SwiperSlide>
+      
+      <SwiperSlide className={css.slide}>
+        <img src={Victoria_min} alt="Victoria Romashenko shares his oppinion" />
+        <h3>Victoria Romashenko</h3>
+            <p>I love how fast and fresh the vegetables are delivered. 
+                Salads always turn out so appetizing and aoups are real masterpieces. The quality is impeccable!</p></SwiperSlide>
+      <SwiperSlide className={css.slide}>
+        <img src={Ihor_min} alt="Ihor Trachuk shares his oppinion" />
+        <h3>Ihor Trachuk</h3>
+            <p>Recently tried my fresh tomatoes and peppers amazing! 
+                The authenticity of the taste, as if from the garden 
+                directly to my table. It is now my main vegetable supplier.</p></SwiperSlide>
+    </Swiper>
+    {/* <ul>
         <li>
             <img src="" alt="" />
             <h3>John Wax</h3>
@@ -24,6 +63,6 @@ export default function ReviewsSection () {
                 The authenticity of the taste, as if from the garden 
                 directly to my table. It is now my main vegetable supplier.</p>
         </li>
-    </ul>
+    </ul> */}
     </div>
 }
